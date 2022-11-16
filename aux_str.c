050098a43729a20ddd2407af07ef0abb28a74900
Anthony Onyedika Chuka-Umeora <110993280+Antonysyl@users.noreply.github.com>
2022-11-16 13:55:06 +0100

    strings

--- aux_str.c
+++ aux_str.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _strcat - concatenate two strings
  * @dest: char pointer the dest of the copied str
@@ -8,20 +7,17 @@
  */
 char *_strcat(char *dest, const char *src)
 {
-	int i;
-	int j;
-
-	for (i = 0; dest[i] != '\0'; i++)
-		;
-
-	for (j = 0; src[j] != '\0'; j++)
-	{
-		dest[i] = src[j];
-		i++;
-	}
-
-	dest[i] = '\0';
-	return (dest);
+int i;
+int j;
+for (i = 0; dest[i] != '\0'; i++)
+;
+for (j = 0; src[j] != '\0'; j++)
+{
+dest[i] = src[j];
+i++;
+}
+dest[i] = '\0';
+return (dest);
 }
 /**
  * *_strcpy - Copies the string pointed to by src.
@@ -31,16 +27,13 @@ char *_strcat(char *dest, const char *src)
  */
 char *_strcpy(char *dest, char *src)
 {
-
-	size_t a;
-
-	for (a = 0; src[a] != '\0'; a++)
-	{
-		dest[a] = src[a];
-	}
-	dest[a] = '\0';
-
-	return (dest);
+size_t a;
+for (a = 0; src[a] != '\0'; a++)
+{
+dest[a] = src[a];
+}
+dest[a] = '\0';
+return (dest);
 }
 /**
  * _strcmp - Function that compares two strings.
@@ -50,16 +43,14 @@ char *_strcpy(char *dest, char *src)
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-
-	for (i = 0; s1[i] == s2[i] && s1[i]; i++)
-		;
-
-	if (s1[i] > s2[i])
-		return (1);
-	if (s1[i] < s2[i])
-		return (-1);
-	return (0);
+int i;
+for (i = 0; s1[i] == s2[i] && s1[i]; i++)
+;
+if (s1[i] > s2[i])
+return (1);
+if (s1[i] < s2[i])
+return (-1);
+return (0);
 }
 /**
  * _strchr - locates a character in a string,
@@ -69,14 +60,13 @@ int _strcmp(char *s1, char *s2)
  */
 char *_strchr(char *s, char c)
 {
-	unsigned int i = 0;
-
-	for (; *(s + i) != '\0'; i++)
-		if (*(s + i) == c)
-			return (s + i);
-	if (*(s + i) == c)
-		return (s + i);
-	return ('\0');
+unsigned int i = 0;
+for (; *(s + i) != '\0'; i++)
+if (*(s + i) == c)
+return (s + i);
+if (*(s + i) == c)
+return (s + i);
+return ('\0');
 }
 /**
  * _strspn - gets the length of a prefix substring.
@@ -86,21 +76,20 @@ char *_strchr(char *s, char c)
  */
 int _strspn(char *s, char *accept)
 {
-	int i, j, bool;
-
-	for (i = 0; *(s + i) != '\0'; i++)
-	{
-		bool = 1;
-		for (j = 0; *(accept + j) != '\0'; j++)
-		{
-			if (*(s + i) == *(accept + j))
-			{
-				bool = 0;
-				break;
-			}
-		}
-		if (bool == 1)
-			break;
-	}
-	return (i);
+int i, j, bool;
+for (i = 0; *(s + i) != '\0'; i++)
+{
+bool = 1;
+for (j = 0; *(accept + j) != '\0'; j++)
+{
+if (*(s + i) == *(accept + j))
+{
+bool = 0;
+break;
+}
+}
+if (bool == 1)
+break;
+}
+return (i);
 }
