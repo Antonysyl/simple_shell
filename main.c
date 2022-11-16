21e4c78d5a111bb32e0351171588cbbbde6a1300
Anthony Onyedika Chuka-Umeora <110993280+Antonysyl@users.noreply.github.com>
2022-11-16 14:30:09 +0100

    data structure

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * free_data - frees data structure
  *
@@ -8,17 +7,14 @@
  */
 void free_data(data_shell *datash)
 {
-	unsigned int i;
-
-	for (i = 0; datash->_environ[i]; i++)
-	{
-		free(datash->_environ[i]);
-	}
-
-	free(datash->_environ);
-	free(datash->pid);
+unsigned int i;
+for (i = 0; datash->_environ[i]; i++)
+{
+free(datash->_environ[i]);
 }
-
+free(datash->_environ);
+free(datash->pid);
+}
 /**
  * set_data - Initialize data structure
  *
@@ -28,28 +24,22 @@ void free_data(data_shell *datash)
  */
 void set_data(data_shell *datash, char **av)
 {
-	unsigned int i;
-
-	datash->av = av;
-	datash->input = NULL;
-	datash->args = NULL;
-	datash->status = 0;
-	datash->counter = 1;
-
-	for (i = 0; environ[i]; i++)
-		;
-
-	datash->_environ = malloc(sizeof(char *) * (i + 1));
-
-	for (i = 0; environ[i]; i++)
-	{
-		datash->_environ[i] = _strdup(environ[i]);
-	}
-
-	datash->_environ[i] = NULL;
-	datash->pid = aux_itoa(getpid());
+unsigned int i;
+datash->av = av;
+datash->input = NULL;
+datash->args = NULL;
+datash->status = 0;
+datash->counter = 1;
+for (i = 0; environ[i]; i++)
+;
+datash->_environ = malloc(sizeof(char *) * (i + 1));
+for (i = 0; environ[i]; i++)
+{
+datash->_environ[i] = _strdup(environ[i]);
 }
-
+datash->_environ[i] = NULL;
+datash->pid = aux_itoa(getpid());
+}
 /**
  * main - Entry point
  *
@@ -60,14 +50,13 @@ void set_data(data_shell *datash, char **av)
  */
 int main(int ac, char **av)
 {
-	data_shell datash;
-	(void) ac;
-
-	signal(SIGINT, get_sigint);
-	set_data(&datash, av);
-	shell_loop(&datash);
-	free_data(&datash);
-	if (datash.status < 0)
-		return (255);
-	return (datash.status);
+data_shell datash;
+(void) ac;
+signal(SIGINT, get_sigint);
+set_data(&datash, av);
+shell_loop(&datash);
+free_data(&datash);
+if (datash.status < 0)
+return (255);
+return (datash.status);
 }
