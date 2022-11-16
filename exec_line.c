c9bfa7dee62a381172644651c5ea85b59845c082
Anthony Onyedika Chuka-Umeora <110993280+Antonysyl@users.noreply.github.com>
2022-11-16 14:23:21 +0100

    finding builtin and commands

--- exec_line.c
+++ exec_line.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * exec_line - finds builtins and commands
  *
@@ -8,15 +7,11 @@
  */
 int exec_line(data_shell *datash)
 {
-	int (*builtin)(data_shell *datash);
-
-	if (datash->args[0] == NULL)
-		return (1);
-
-	builtin = get_builtin(datash->args[0]);
-
-	if (builtin != NULL)
-		return (builtin(datash));
-
-	return (cmd_exec(datash));
+int (*builtin)(data_shell *datash);
+if (datash->args[0] == NULL)
+return (1);
+builtin = get_builtin(datash->args[0]);
+if (builtin != NULL)
+return (builtin(datash));
+return (cmd_exec(datash));
 }
